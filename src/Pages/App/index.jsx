0575bc15ad1ec57c7f1f3a94c49b5d56c698ade4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-04 00:22:30 -0400

    feat: Add MyAccount, MyOrder, MyOrders, NotFound, and SignIn page components

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,9 @@
 import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
 import './App.css'
 
 function App() {
