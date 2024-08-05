319698ac6c6be1c9cca55b5e9f0b8dcec1eb1da5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-05 13:24:29 -0400

    feat: Add sign-in page component and update AppRoutes in App component

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -13,8 +13,8 @@ const AppRoutes = () =>{
     { path: '/my-account', element: <MyAccount /> },
     { path: '/my-order', element: <MyOrder /> },
     { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
     { path: '/*', element: <NotFound /> },
-    { path: '/sign-in', element: <SignIn /> }
   ])
 
   return routes
