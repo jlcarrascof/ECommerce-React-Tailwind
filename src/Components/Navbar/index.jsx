a72af1fafbb55e7eddb5d7c0efccb7665cfa0879
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-05 22:06:50 -0400

    feat: Add Navlink component to Navbar for navigation links

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,4 +1,14 @@
+import { Navlink } from 'react-router-dom'
 const Navbar = () => {
     return (
+        <nav>
+            <ul>
+                <li>
+                    <Navlink>
+
+                    </Navlink>
+                </li>
+            </ul>
+        </nav>
     )
 }
