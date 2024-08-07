838feac3618a42428f4601acc1001c4fcf35ee9c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-07 12:54:21 -0400

    Refactor Navbar component to include fixed positioning and update navigation links

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -10,7 +10,9 @@ const Navbar = () => {
         <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
-                    <NavLink to='/'>
+                    <NavLink to='/'
+                        style={({ isActive }) => isActive ? activeStyle : undefined}
+                    >
                         Shopi
                     </NavLink>
                 </li>
