5ab8da189ae1a7a451bf6e4f2ebe55d830e006bf
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-06 21:29:15 -0400

    feat: Add new navigation links to Navbar component

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -39,6 +39,43 @@ const Navbar = () => {
                     </Navlink>
                 </li>
             </ul>
+            <ul>
+                <li>
+                    <Navlink to='/'>
+                        Shopi
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/'>
+                        All
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/clothes'>
+                        Clothes
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/electronics'>
+                        Electronics
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/furnitures'>
+                        Furnitures
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/toys'>
+                        Toys
+                    </Navlink>
+                </li>
+                <li>
+                    <Navlink to='/others'>
+                        Others
+                    </Navlink>
+                </li>
+            </ul>
         </nav>
     )
 }
