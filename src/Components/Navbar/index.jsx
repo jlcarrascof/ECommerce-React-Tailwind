7f8188e026e509222af618be7db30e89fd8e06c5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-07 13:52:38 -0400

    Refactor Navbar component to use NavLink for navigation links and update styling

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,16 +2,13 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
-    const activeStyle = 'underline'
+    const activeStyle = 'underline underline-offset-4'
 
     return (
         <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
-                    <NavLink
-                        to='/'
-                        className={({ isActive }) => isActive ? activeStyle : undefined}
-                    >
+                    <NavLink to='/'>
                         Shopi
                     </NavLink>
                 </li>
