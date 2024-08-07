a0504055f98a4580612c677ed064cdd4d6cbaf37
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-06 23:02:41 -0400

    Refactor Navbar component to use smaller gap between items in flex layout

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
-            <ul className='flex items-center gap-8'>
-                <li className=''>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'>
                     <NavLink to='/'>
                         Shopi
                     </NavLink>
