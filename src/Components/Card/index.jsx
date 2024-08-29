07d5d433ed6917d23b057b42414d1fa51bfdce72
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 12:10:31 -0400

    feat: Add Card component for displaying product information

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+    return (
+        <div>
+            <figure>
+                <span></span>
+                <img src="" alt="" />
+                <div>+</div>
+            </figure>
+            <p>
+                <span>Headphones</span>
+                <span>$300</span>
+            </p>
+        </div>
+    )
+}
+
+export default Card
