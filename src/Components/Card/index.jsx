52b77dbd0731d9db5d0b49893873d708927c9ed2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 12:38:18 -0400

    Refactor Card component to improve styling and add dynamic content

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,8 +1,8 @@
 const Card = () => {
     return (
-        <div>
-            <figure>
-                <span></span>
+        <div className='bg-white cursor-pointer w-56 h-60'>
+            <figure className='relative mb-2 w-full h-4/5'>
+                <span className='absolute'>Electronics</span>
                 <img src="" alt="" />
                 <div>+</div>
             </figure>
