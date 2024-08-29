9d8a62dc0790ed91231cef3cf63503ca2f1fe7d5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 14:31:48 -0400

    Refactor Card component to use smaller text size for category label

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,9 +2,9 @@ const Card = () => {
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm'>Electronics</span>
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2'>Electronics</span>
                 <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full'>
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2'>
                     +
                 </div>
             </figure>
