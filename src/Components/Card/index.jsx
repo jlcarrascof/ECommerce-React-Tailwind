e6c4860108eaf560f447bf2dc1a366c050f04ba9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 14:26:36 -0400

    Refactor Card component to use object-fit: cover for image styling

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,7 +3,7 @@ const Card = () => {
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm'>Electronics</span>
-                <img src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
+                <img className='w-full h-full object-cover' src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full'>
                     +
                 </div>
