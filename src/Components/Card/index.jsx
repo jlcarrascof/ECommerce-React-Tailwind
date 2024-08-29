f02605937dcbd23f315b0ae77df8756a043362a9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 12:44:17 -0400

    Refactor Card component styling last element in figure

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,8 +3,10 @@ const Card = () => {
         <div className='bg-white cursor-pointer w-56 h-60'>
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute'>Electronics</span>
-                <img src="" alt="" />
-                <div>+</div>
+                <img src='' alt='headphones' />
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full'>
+                    +
+                </div>
             </figure>
             <p>
                 <span>Headphones</span>
