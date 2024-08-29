d6c68dacaa5e05f6ead6d7a4af1392a32c277046
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 14:24:33 -0400

    chore: Refactor Card component styling and add dynamic content

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,16 @@
 const Card = () => {
     return (
-        <div className='bg-white cursor-pointer w-56 h-60'>
+        <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute'>Electronics</span>
-                <img src='' alt='headphones' />
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm'>Electronics</span>
+                <img src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full'>
                     +
                 </div>
             </figure>
-            <p>
-                <span>Headphones</span>
-                <span>$300</span>
+            <p className='flex justify-between'>
+                <span className='text-sm font-light'>Headphones</span>
+                <span className='text-lg font-medium'>$300</span>
             </p>
         </div>
     )
