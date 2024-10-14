bf244488a50e13d736758cd9a396deacb40c27e2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-14 19:18:19 -0400

    Refactor Home component to use grid layout for displaying items

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -15,11 +15,13 @@ function Home() {
     return (
       <Layout>
         Home
-        {
-          items?.map(item => (
-            <Card key={item.id} data={item} />
-          ))
-        }
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map(item => (
+              <Card key={item.id} data={item} />
+            ))
+          }
+        </div>
       </Layout>
     )
 }
