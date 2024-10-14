cb7834214d4c1fcec44bbb3924e7bde1b27da69f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-14 14:05:40 -0400

    Refactor Home component to add useEffect hook for fetching items

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
 
     const [items, setItems] = useState(null)
+
+    useEffect(() => {
+
+    }, [])
 
     return (
       <Layout>
