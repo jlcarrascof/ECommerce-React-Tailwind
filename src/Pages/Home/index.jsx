0c65a7e07989f11c260af448d4389bb66e0cc2fc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-14 14:02:52 -0400

    Refactor Home component to add state for items

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
+
+    const [items, setItems] = useState(null)
+
     return (
       <Layout>
         Home
